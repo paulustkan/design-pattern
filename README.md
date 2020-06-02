a7e17da24a919de1c85e31ca1bfe3f047ba795af
paulustkan <paulustkan@gmail.com>
2020-06-02 16:01:38 +0700

    add decorator and regular expression

--- README.md
+++ README.md
@@ -1,12 +1,118 @@
-## design-pattern
+## Link references
 
-# composite
+https://refactoring.guru/
+https://regexr.com/
+https://regexone.com/
+
+## Design pattern
+
+# Composite
 - Get sum of all items in recursion tree
 - Use abstract class
 - Delegate the work to its sub-elements
 - Applicability: create form element
 
-# decorator
+# Decorator
 - Add new behavor following an object by wrapper
 - Use interface
 - Applicability: check and remove unnecessary texts in content
+
+## Regular Expression (Javascript)
+
+# A character
+
+`[aeiou]`
+get any character that inside []
+
+`[^aeiou]`
+get any character that without inside []
+
+`[g-s]`
+get any character that in range g -> s
+
+`.`
+`^\n\r]`
+get any character except linebreaks
+
+`\s\S]`
+get any character including linebreaks
+
+`\w`
+get any character that is alphanumeric and underscore
+
+`\W`
+get any character that is not alphanumeric and underscore
+
+`\d`
+`[0-9]`
+get any character that is number
+
+`\D`
+`[^0-9]`
+get any character that is not number
+
+`\s`
+matches any whitespace character (spaces, tabs, line breaks)
+
+`\S`
+matches any character that is not whitespace
+
+# A position (not a character)
+
+`^\w`
+`^` beginning by the word
+
+`\w$`
+`$` ending by the word
+
+`word_you_want\b`
+matches position that following word_you_want is not word (alphanumeric and underscore)
+
+`word_you_want\B`
+matches position that is not a word boundary
+
+# Escaped character
+
+`\t`
+tab
+
+`\+`
+plus
+
+...
+
+# Groups & References
+
+`(abc)`
+matches words that is abc
+
+`(abc)+`
+matches words that is abc or abcabc, abc... (one or more abc)
+
+# Quantifiers & Alternation
+
+`colou?r`
+`?` matches 0 or 1 of the preceding token (will `color`, `colour`)
+
+`b\w+`
+`+` matches 1 or more of the preceding token (`\w` has 1 or more)
+
+`b\w*`
+`*` matches 0 or more of the preceding token (`\w` has 0 or more)
+
+`+?`
+combines `+` and `?`, to get 1
+
+`b\w{2,3}`
+`{2,3}` matches between 2 and 3 of the preceding token (`\w` has 2 or 3)
+`{2}` exactly 2
+`{2,}` 2 or more
+
+`b(a|e|i)d`
+`|` like a boolean OR (will get `bad`, `bed`, `bid`)
+
+# Substitution
+
+# Lookaround
+
+# Flags
--- decorator/real.php
+++ decorator/real.php
@@ -1,2 +1,98 @@
 <?php
 
+interface InputFormat
+{
+    public function formatText(string $text): string;
+}
+
+class TextInput implements InputFormat
+{
+    public function formatText(string $text): string
+    {
+        return $text;
+    }
+}
+
+class TextFormat implements InputFormat
+{
+    protected $inputFormat;
+
+    public function __construct(InputFormat $inputFormat)
+    {
+        $this->inputFormat = $inputFormat;
+    }
+
+    public function formatText(string $text): string
+    {
+        return $this->inputFormat->formatText($text);
+    }
+}
+
+class PlainTextFilter extends TextFormat
+{
+    public function formatText(string $text): string
+    {
+        $text = parent::formatText($text);
+
+        return strip_tags($text);
+    }
+}
+
+class DangerousHTMLTagsFilter extends TextFormat
+{
+    private $dangerousTagPatterns = [
+        "|<script.*?>([\s\S]*)?</script>|i", // ...
+    ];
+
+    private $dangerousAttributes = [
+        "onclick", "onkeypress", // ...
+    ];
+
+
+    public function formatText(string $text): string
+    {
+        $text = parent::formatText($text);
+
+        foreach ($this->dangerousTagPatterns as $pattern) {
+            $text = preg_replace($pattern, '', $text);
+        }
+
+        foreach ($this->dangerousAttributes as $attribute) {
+            $text = preg_replace_callback('|<(.*?)>|', function ($matches) use ($attribute) {
+                $result = preg_replace("|$attribute=|i", '', $matches[1]);
+                return "<" . $result . ">";
+            }, $text);
+        }
+
+        return $text;
+    }
+}
+
+function displayCommentAsAWebsite(InputFormat $format, string $text)
+{
+    echo $format->formatText($text);
+}
+
+$dangerousComment = <<<HERE
+Hello! Nice blog post!
+Please visit my <a href='http://www.iwillhackyou.com'>homepage</a>.
+<script src="http://www.iwillhackyou.com/script.js">
+    performXSSAttack();
+</script>
+HERE;
+
+/**
+ * Naive comment rendering (unsafe).
+ */
+$naiveInput = new TextInput;
+echo "Website renders comments without filtering (unsafe):<br />";
+displayCommentAsAWebsite($naiveInput, $dangerousComment);
+echo "<br /><br /><br />";
+
+/**
+ * Filtered comment rendering (safe).
+ */
+$filteredInput = new PlainTextFilter($naiveInput);
+echo "Website renders comments after stripping all tags (safe):<br />";
+displayCommentAsAWebsite($filteredInput, $dangerousComment);
+echo "<br /><br /><br />";
